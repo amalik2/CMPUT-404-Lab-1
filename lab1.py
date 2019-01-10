8e74d77cd497284f656cd6ba1daa88875ae043ba
Adil Malik <malik2@cs.ualberta.ca>
2019-01-09 17:54:48 -0700

    print out link to python script

--- lab1.py
+++ lab1.py
@@ -4,3 +4,6 @@ print(requests.__version__)
 r = requests.get("http://www.google.com")
 #print(r.status_code)
 #print(r.text)
+
+a = requests.get("https://raw.githubusercontent.com/amalik2/CMPUT-404-Lab-1/master/lab1.py")
+print(a.text)
